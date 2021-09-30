c9b7fccf623946b2d005afa8015d0080f36c5136
Basquiat <ememdaniel98@gmail.com>
2021-09-30 17:56:17 -0500

    changed to imperial metrics, added styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,19 @@ var saveBtn = $('#saveBtn');
 var cityToSearch = $("#city-search").val();
 var addName = $("#city-name");
 var addTemp = $("#temp");
-var addHumidity = $("humidity");
+var h = $("#h");
 var addWind = $("#wind")
-
-console.log(cityToSearch);
-
-
 
 saveBtn.on('click', function(event) {
     event.preventDefault();
-
     getWeatherData();
-    addName.css('display', 'none');
+
 });
 
 function getWeatherData() {
     cityToSearch = $("#city-search").val();
-    console.log(cityToSearch);
     var APIkey = "3eaff4dcea866e57c4766356b8dd3f28";
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + APIkey;
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&units=imperial&appid=" + APIkey;
     console.log(requestUrl);
     fetch(requestUrl)
         .then(function(response) {
@@ -29,12 +23,11 @@ function getWeatherData() {
                     var cityName = cityToSearch;
                     var cityTemp = data.main.temp;
                     var cityHumidity = data.main.humidity;
-                    var wind = data.wind.speed;
+                    var cityWind = data.wind.speed;
                     addName.append(cityName);
                     addTemp.append(cityTemp);
-                    addHumidity.append(cityHumidity);
-                    addWind.append(wind);
-                    console.log(humidity);
+                    h.append(cityHumidity);
+                    addWind.append(cityWind);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -44,6 +37,7 @@ function getWeatherData() {
             alert('Unable to connect to Weather Dashboard');
         });
 };
+
 
 // function displayCurrentCityDay() {
 
