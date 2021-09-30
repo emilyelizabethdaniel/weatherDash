df418d7fe6a2b241ab73e479cf4ff4a3ad6c989f
Basquiat <ememdaniel98@gmail.com>
2021-09-30 13:03:06 -0500

    got the api to work, feelin goood

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 var saveBtn = $('#saveBtn');
 var cityToSearch = $("#city-search").val();
-// var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=' + cityToSearch + '&appid=3eaff4dcea866e57c4766356b8dd3f28';
-// console.log(requestUrl);
 
-
+console.log(cityToSearch);
 
 saveBtn.on('click', function(event) {
     event.preventDefault();
+    getWeatherData();
+});
+
+function getWeatherData() {
     cityToSearch = $("#city-search").val();
     console.log(cityToSearch);
-    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=' + cityToSearch + '&appid=3eaff4dcea866e57c4766356b8dd3f28';
+    var APIkey = "3eaff4dcea866e57c4766356b8dd3f28";
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&appid=" + APIkey;
     console.log(requestUrl);
     fetch(requestUrl)
         .then(function(response) {
-            return response.json();
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data, cityToSearch);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
         })
-        .then(function(data) {
-            console.log(data)
-        })
-});
+        .catch(function(error) {
+            alert('Unable to connect to Weather Dashboard');
+        });
+}
