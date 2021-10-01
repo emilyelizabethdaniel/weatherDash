cd6d6c81e70b1864a8dae61b4a16beb7d34f2345
Basquiat <ememdaniel98@gmail.com>
2021-09-30 19:03:43 -0500

    got second api pull for the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,22 +4,34 @@ var addName = $("#city-name");
 var addTemp = $("#temp");
 var h = $("#h");
 var addWind = $("#wind")
+var dailyCityWeather = $('#daily-city-weather');
+var savedCities = $('saved-cities');
+var APIkey = "3eaff4dcea866e57c4766356b8dd3f28";
+
+
+
 
 saveBtn.on('click', function(event) {
     event.preventDefault();
+    // dailyCityWeather.h3.remove();
+    // dailyCityWeather.p.remove();
     getWeatherData();
-
 });
+
 
 function getWeatherData() {
     cityToSearch = $("#city-search").val();
-    var APIkey = "3eaff4dcea866e57c4766356b8dd3f28";
     var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityToSearch + "&units=imperial&appid=" + APIkey;
     console.log(requestUrl);
+    var fiveDayRequest = "https://api.openweathermap.org/data/2.5/forecast?q= " + cityToSearch + "&appid=" + APIkey;
+
+    /////////////////////////////////////////////////////////////CURRENT DAY REQUEST/////////////////////
+
     fetch(requestUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
+                    console.log(data);
                     var cityName = cityToSearch;
                     var cityTemp = data.main.temp;
                     var cityHumidity = data.main.humidity;
@@ -28,6 +40,24 @@ function getWeatherData() {
                     addTemp.append(cityTemp);
                     h.append(cityHumidity);
                     addWind.append(cityWind);
+
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function(error) {
+            alert('Unable to connect to Weather Dashboard');
+
+        });
+
+    ///////////////////////////////////////////////////////////////////FIVE DAY REQUEST////////////////////
+
+    fetch(fiveDayRequest)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -36,7 +66,12 @@ function getWeatherData() {
         .catch(function(error) {
             alert('Unable to connect to Weather Dashboard');
         });
+
 };
+
+var fiveDayRequest = "https://api.openweathermap.org/data/2.5/forecast?q= " + cityToSearch + "&appid=" +
+    APIkey;
+
 
 
 // function displayCurrentCityDay() {
